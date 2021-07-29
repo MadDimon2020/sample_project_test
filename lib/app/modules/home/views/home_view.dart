f93d085e35b6b5ef442289167dfc9d1cb6bb806d
Dmitry <dmitrymelkozerov@gmail.com>
2021-07-29 23:27:23 +0500

    Forgot to import cupertino.dart

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -51,188 +52,192 @@ class HomeView extends GetView<HomeController> {
           }
           var fetchedNews =
               NewsFeed$SubscriptionRoot.fromJson(result.data).news;
-          return ListView.builder(
-            itemBuilder: (context, index) {
-              return Container(
-                height: _masterContainerHeight,
-                width: _masterContainerWidth,
-                margin: EdgeInsets.only(
-                  left: _masterContainerMargin,
-                  right: _masterContainerMargin,
-                  top: _masterContainerMargin * 2,
-                ),
-                child: InkWell(
-                  onTap: () {
-                    Get.toNamed(Routes.POST_DETAILS,
-                        arguments: [fetchedNews[index].id]);
-                  },
-                  splashColor: Theme.of(context).primaryColor,
-                  child: Card(
-                    color: Colors.grey[200],
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0)),
-                    elevation: 5.0,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Flexible(
-                          flex: 80,
-                          child: Row(children: [
-                            Flexible(
-                              flex: 70,
-                              child: Column(
-                                children: [
-                                  Flexible(
-                                    flex: 70,
-                                    child: Container(
-                                      margin: EdgeInsets.only(
-                                        top: _masterContainerWidth * 0.03,
-                                        left: _masterContainerWidth * 0.04,
-                                        right: _masterContainerWidth * 0.04,
-                                      ),
-                                      child: Align(
-                                        alignment: Alignment.centerLeft,
-                                        child: Text(
-                                          fetchedNews[index].title,
-                                          style: TextStyle(
-                                            fontSize:
-                                                _masterContainerHeight * 0.10,
-                                            fontWeight: FontWeight.bold,
+          return CupertinoScrollbar(
+            isAlwaysShown: true,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Container(
+                  height: _masterContainerHeight,
+                  width: _masterContainerWidth,
+                  margin: EdgeInsets.only(
+                    left: _masterContainerMargin,
+                    right: _masterContainerMargin,
+                    top: _masterContainerMargin * 2,
+                  ),
+                  child: InkWell(
+                    onTap: () {
+                      Get.toNamed(Routes.POST_DETAILS,
+                          arguments: [fetchedNews[index].id]);
+                    },
+                    splashColor: Theme.of(context).primaryColor,
+                    child: Card(
+                      color: Colors.grey[200],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0)),
+                      elevation: 5.0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            flex: 80,
+                            child: Row(children: [
+                              Flexible(
+                                flex: 70,
+                                child: Column(
+                                  children: [
+                                    Flexible(
+                                      flex: 70,
+                                      child: Container(
+                                        margin: EdgeInsets.only(
+                                          top: _masterContainerWidth * 0.03,
+                                          left: _masterContainerWidth * 0.04,
+                                          right: _masterContainerWidth * 0.04,
+                                        ),
+                                        child: Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: Text(
+                                            fetchedNews[index].title,
+                                            style: TextStyle(
+                                              fontSize:
+                                                  _masterContainerHeight * 0.10,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                            maxLines: 4,
+                                            softWrap: true,
+                                            overflow: TextOverflow.ellipsis,
                                           ),
-                                          maxLines: 4,
-                                          softWrap: true,
-                                          overflow: TextOverflow.ellipsis,
                                         ),
                                       ),
                                     ),
-                                  ),
-                                  Flexible(
-                                    flex: 30,
-                                    child: Container(
-                                      margin: EdgeInsets.symmetric(
-                                        vertical: _masterContainerHeight * 0.04,
-                                        horizontal:
-                                            _masterContainerWidth * 0.04,
-                                      ),
-                                      child: Align(
-                                        alignment: Alignment.topLeft,
-                                        child: Text(
-                                          fetchedNews[index].content,
-                                          style: TextStyle(
-                                            fontSize:
-                                                _masterContainerHeight * 0.07,
+                                    Flexible(
+                                      flex: 30,
+                                      child: Container(
+                                        margin: EdgeInsets.symmetric(
+                                          vertical:
+                                              _masterContainerHeight * 0.04,
+                                          horizontal:
+                                              _masterContainerWidth * 0.04,
+                                        ),
+                                        child: Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Text(
+                                            fetchedNews[index].content,
+                                            style: TextStyle(
+                                              fontSize:
+                                                  _masterContainerHeight * 0.07,
+                                            ),
+                                            maxLines: 2,
+                                            softWrap: true,
+                                            overflow: TextOverflow.ellipsis,
                                           ),
-                                          maxLines: 2,
-                                          softWrap: true,
-                                          overflow: TextOverflow.ellipsis,
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                            Flexible(
-                              flex: 30,
-                              child: Column(
-                                children: [
-                                  Flexible(
-                                    flex: 75,
-                                    child: Container(
-                                      margin: EdgeInsets.only(
-                                        top: _masterContainerWidth * 0.03,
-                                        right: _masterContainerWidth * 0.03,
-                                      ),
-                                      child: Align(
-                                        alignment: Alignment.topCenter,
-                                        child: ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(15.0),
-                                          child: Image.asset(
-                                            'assets/images/user-image-placeholder.jpg',
-                                            fit: BoxFit.cover,
+                              Flexible(
+                                flex: 30,
+                                child: Column(
+                                  children: [
+                                    Flexible(
+                                      flex: 75,
+                                      child: Container(
+                                        margin: EdgeInsets.only(
+                                          top: _masterContainerWidth * 0.03,
+                                          right: _masterContainerWidth * 0.03,
+                                        ),
+                                        child: Align(
+                                          alignment: Alignment.topCenter,
+                                          child: ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(15.0),
+                                            child: Image.asset(
+                                              'assets/images/user-image-placeholder.jpg',
+                                              fit: BoxFit.cover,
+                                            ),
                                           ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                  Flexible(
-                                    flex: 25,
-                                    child: Container(
-                                      child: Align(
-                                        child: ElevatedButton(
-                                          style: ButtonStyle(
-                                              padding: MaterialStateProperty
-                                                  .all<EdgeInsets>(
-                                                EdgeInsets.symmetric(
-                                                    horizontal:
-                                                        deviceWidth * 0.04),
-                                              ),
-                                              backgroundColor:
-                                                  MaterialStateProperty.all<
-                                                      Color>(Colors.green),
-                                              shape: MaterialStateProperty.all<
-                                                  OutlinedBorder>(
-                                                RoundedRectangleBorder(
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          15.0),
+                                    Flexible(
+                                      flex: 25,
+                                      child: Container(
+                                        child: Align(
+                                          child: ElevatedButton(
+                                            style: ButtonStyle(
+                                                padding: MaterialStateProperty
+                                                    .all<EdgeInsets>(
+                                                  EdgeInsets.symmetric(
+                                                      horizontal:
+                                                          deviceWidth * 0.04),
                                                 ),
-                                              )),
-                                          child: Text('SAVE'),
-                                          onPressed: () {
-                                            print('SAVE-Button pressed');
-                                          },
+                                                backgroundColor:
+                                                    MaterialStateProperty.all<
+                                                        Color>(Colors.green),
+                                                shape: MaterialStateProperty
+                                                    .all<OutlinedBorder>(
+                                                  RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            15.0),
+                                                  ),
+                                                )),
+                                            child: Text('SAVE'),
+                                            onPressed: () {
+                                              print('SAVE-Button pressed');
+                                            },
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ]),
-                        ),
-                        Flexible(
-                          flex: 20,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Flexible(
-                                flex: 55,
-                                child: Container(
-                                  child: Align(
-                                    child: Text(
-                                      '${DateFormat('EEEE, dd LLLL yyyy').format(DateTime.parse(fetchedNews[index].createdAt.toString()))}',
-                                      style: TextStyle(
-                                          fontSize:
-                                              _masterContainerHeight * 0.07),
-                                    ),
-                                  ),
+                                  ],
                                 ),
                               ),
-                              Flexible(
-                                flex: 45,
-                                child: Container(
-                                  child: Align(
-                                    child: Text(
-                                      'Posted by {author name}',
-                                      style: TextStyle(
-                                          fontSize:
-                                              _masterContainerHeight * 0.07),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
+                            ]),
                           ),
-                        ),
-                      ],
+                          Flexible(
+                            flex: 20,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Flexible(
+                                  flex: 55,
+                                  child: Container(
+                                    child: Align(
+                                      child: Text(
+                                        '${DateFormat('EEEE, dd LLLL yyyy').format(DateTime.parse(fetchedNews[index].createdAt.toString()))}',
+                                        style: TextStyle(
+                                            fontSize:
+                                                _masterContainerHeight * 0.07),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Flexible(
+                                  flex: 45,
+                                  child: Container(
+                                    child: Align(
+                                      child: Text(
+                                        'Posted by {author name}',
+                                        style: TextStyle(
+                                            fontSize:
+                                                _masterContainerHeight * 0.07),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-            itemCount: fetchedNews.length,
+                );
+              },
+              itemCount: fetchedNews.length,
+            ),
           );
         },
       ),
