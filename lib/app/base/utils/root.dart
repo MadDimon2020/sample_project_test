c8b80e5cbed57f74d592db503187fd6b720b2268
Dmitry <dmitrymelkozerov@gmail.com>
2021-07-29 23:24:56 +0500

    ListView,builder wrapped by Cupertino Scrollbar. Log output added to the Root widget

--- lib/app/base/utils/root.dart
+++ lib/app/base/utils/root.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sample_project/app/modules/auth/views/auth_view.dart';
@@ -7,6 +9,8 @@ import 'package:sample_project/controllers/api_controller.dart';
 class Root extends GetWidget<ApiController> {
   @override
   Widget build(BuildContext context) {
+    log('The token expires in ${ApiController.to.token.value.expiresIn}',
+        name: 'Root');
     return Obx(() {
       return ApiController.to.token.value == null ||
               ApiController.to.token.value.expiresIn <= 0
